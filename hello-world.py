7f91b28c29224a8f0caa384f754982ad3e015a95
kaunjovi <kaunjovi@gmail.com>
2022-06-30 23:26:58 +0530

    get the hello world in.

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,15 @@
+## Using pipenv 
+# kaunjovi@devbook learn-python3-2022 % brew list --version pipenv
+# pipenv 2022.6.7
+# 
+## Uses python of pipenv in this case 3.10
+# kaunjovi@devbook learn-python3-2022 % pipenv run which python 
+# /Users/kaunjovi/code/learn-python3-2022/.venv/bin/python
+# kaunjovi@devbook learn-python3-2022 % pipenv run python --version 
+# Python 3.10.2
+# 
+## And there is the hello world. 
+# kaunjovi@devbook learn-python3-2022 % prp hello-world.py 
+# Hello world from Python 3
+
 print(f"Hello world from Python 3")
